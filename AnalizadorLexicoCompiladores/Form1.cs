bd66d93769c334c957f8e3cf72a690d262783fae
kevenly29 <56766471+kevenly29@users.noreply.github.com>
2023-02-05 22:03:02 -0400

    fuente c#

---
+++ AnalizadorLexicoCompiladores/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalizadorLexicoCompiladores
+{
+    public partial class Form1 : Form
+    {
+        Lexico Analiza_Lexico = new Lexico(); //llamado de la clase
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLexico_Click(object sender, EventArgs e)
+        {
+            Analiza_Lexico.Inicia();
+            
+            Analiza_Lexico.Analiza(textBox1_codigo.Text);
+            TOKEN_LEXEMA.Rows.Clear();
+            if (Analiza_Lexico.NoTokens > 0) 
+                TOKEN_LEXEMA.Rows.Add(Analiza_Lexico.NoTokens);
+            for (int i = 0; i < Analiza_Lexico.NoTokens; i++) 
+            { 
+                TOKEN_LEXEMA.Rows[i].Cells[0].Value = Analiza_Lexico.Token[i];
+                TOKEN_LEXEMA.Rows[i].Cells[1].Value = Analiza_Lexico.Lexema[i]; 
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+                //Limpiar el Datagrid
+               TOKEN_LEXEMA.Rows.Clear();
+                //Limpiar el TextBoxt
+                textBox1_codigo.Text ="";
+                textBox1_codigo.Focus();
+             
+            }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void TOKEN_LEXEMA_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnanalisisSintactico_Click(object sender, EventArgs e)
+        {
+            Analiza_Lexico.Inicia();
+
+            Analiza_Lexico.Analiza(textBox1_codigo.Text);
+            TOKEN_LEXEMA.Rows.Clear();
+            if (Analiza_Lexico.NoTokens > 0)
+                TOKEN_LEXEMA.Rows.Add(Analiza_Lexico.NoTokens);
+            for (int i = 0; i < Analiza_Lexico.NoTokens; i++)
+            {
+                TOKEN_LEXEMA.Rows[i].Cells[0].Value = Analiza_Lexico.Token[i];
+                TOKEN_LEXEMA.Rows[i].Cells[1].Value = Analiza_Lexico.Lexema[i];
+            }
+            /*oAnaSintAscSLR.Inicia(); 
+             if (oAnaSintAscSLR.Analiza(oAnaLex) == 0) label2.Text = "ANALISIS SINTACTICO EXITOSO"; 
+
+            else
+            label2.Text = "ERROR DE SINTAXIS"; */
+        }
+    }
+}
---
+++ AnalizadorLexicoCompiladores/Lexico.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalizadorLexicoCompiladores
+{
+    class Lexico
+    {
+        const int TOKREC = 6;
+        const int MAXTOKENS = 500;
+        string[] _lexemas;
+        string[] _tokens;
+        string _lexema;
+        int _noTokens;
+        int _i;
+        int _iniToken;
+        Automata oAFD;
+        public Lexico()// constructor por defecto
+        { 
+            _lexemas =new string[MAXTOKENS]; 
+            _tokens =new string[MAXTOKENS];
+            oAFD =new Automata(); 
+            _i = 0;
+            _iniToken = 0; 
+            _noTokens = 0;
+        }
+        public int NoTokens
+        {
+            get {return _noTokens; }
+        }
+        public string[] Lexema
+        {
+            get {return _lexemas; }
+        }
+        public string[] Token
+        {
+            get {return _tokens; }
+
+
+ }
+    public void Inicia() 
+        { 
+            _i = 0; 
+            _iniToken = 0; 
+            _noTokens = 0; 
+        }
+        public void Anade(string valTok, string valLex) 
+        {
+            _tokens[_noTokens] = valTok;
+            _lexemas[_noTokens++] = valLex; 
+        }
+        public void Analiza(string texto)
+    {
+        bool recAuto;
+        int noAuto;
+        while (_i < texto.Length)
+        {
+            recAuto = false; 
+            noAuto = 0;
+                for (; noAuto < TOKREC && !recAuto;) 
+                    if (oAFD.Reconoce(texto, _iniToken, ref _i, noAuto)) 
+                        recAuto = true;
+                else
+                        noAuto++;
+                if (recAuto)
+            {
+                _lexema = texto.Substring(_iniToken, _i - _iniToken);
+                    switch (noAuto)
+                {
+                       // --------------Automata  delim--------------
+                       case 0: // _tokens[_noTokens] = "delim";
+                               break;
+                            //--------------  Automata  Id--------------
+                            case 1: if (EsId())    
+                                _tokens[_noTokens] = "id"; 
+                            else  
+                                _tokens[_noTokens] = _lexema; 
+                            break; 
+                            //--------------  Automata  real--------------
+                            case 2: _tokens[_noTokens] = "real"; 
+                            break; 
+                            //--------------  Automata  entero--------------
+                            case 3: _tokens[_noTokens] = "entero";
+                            break; 
+                            //--------------  Automata  cad--------------
+                            case 4: _tokens[_noTokens] = "cad"; 
+                            break;
+                            //--------------  Automata  otros--------------
+                            case 5: _tokens[_noTokens] = "otros"; 
+                            break;
+                    }
+                    if (noAuto != 0) 
+                        _lexemas[_noTokens++] = _lexema;
+                }
+                else 
+                    _i++; 
+                _iniToken = _i;
+            }
+        }
+        private bool EsId()
+
+
+ { 
+            string[] palres = { "inicio", "fin", "const", "var", "entero", "real", "cadena", "leer", "visua" };
+            for (int i = 0; i < palres.Length; i++) 
+                if (_lexema == palres[i]) 
+                    return false;
+            return true;
+        }
+  }//Fin de la clase Lexico
+
+
+        }
+    
